95e5971f017807bad587235a284a8bc3e58776a7
Aman-Deol <deolaman22@gmail.com>
2016-04-17 21:50:00 -0700

    added finalscene assets and bat scripts

---
+++ HauntedMimer/Assets/Scripts/BatTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatTrigger : MonoBehaviour {
+	//public BatFinalMove script;
+	public BatFinalMove[] scripts;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		// play a sound needed
+		foreach(BatFinalMove scr in scripts){
+			scr.BatStart ();
+		}
+	}
+}
