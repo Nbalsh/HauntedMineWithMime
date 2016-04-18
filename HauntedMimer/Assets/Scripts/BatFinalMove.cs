208c8fe4df1af4e677fde3c9d33b9fa79f474a2d
Aman-Deol <deolaman22@gmail.com>
2016-04-17 20:33:11 -0700

    Added bat scripts
    Added a trigger and bat movement along z axis

---
+++ HauntedMimer/Assets/Scripts/BatFinalMove.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatFinalMove : MonoBehaviour {
+	public int speed = 50;
+	public int spaces = 50;
+	// Use this for initialization
+	void Start () {
+		//float startingZpos = this.transform.position.z;
+		//float startingZpos2 = GameObject.FindGameObjectWithTag("Bat").transform.position.z;
+		//MoveInvoker ();
+		//StartCoroutine("ContinuousMove", startingZpos);
+		//StartCoroutine("ContinuousMoveObject", startingZpos2);
+	}
+	
+	void Update () {
+		//this.GetComponent<Transform> ().Translate(0,0,Time.deltaTime * speed * -1);
+	}
+
+	// called by the trigger cube
+	public void BatStart(){
+		float startingZpos = this.transform.position.z;
+		//float startingZpos2 = GameObject.FindGameObjectWithTag("Bat").transform.position.z;
+		//MoveInvoker ();
+		StartCoroutine("ContinuousMove", startingZpos);
+		//StartCoroutine("ContinuousMoveObject", startingZpos2);
+	}
+
+	void MoveInvoker(int startingZpos)
+	{
+		StartCoroutine(ContinuousMove(startingZpos));
+	}
+
+	// Used to move the attached object
+	IEnumerator ContinuousMove(float z)
+	{
+		var finalZ = z - spaces;
+		while (this.transform.position.z > finalZ) {
+			yield return new WaitForSeconds(0F);
+			this.GetComponent<Transform> ().Translate(0,0,Time.deltaTime * speed * -1);
+			/*z--;
+			if (this.transform.position.z == finalZ) {
+				this.GetComponent<Renderer> ().enabled = false;
+			}*/
+		}
+		this.GetComponent<Renderer> ().enabled = false;
+	}
+
+	// used to move all bats
+	// not used
+	IEnumerator ContinuousMoveObject(float z)
+	{
+		var finalZ = z - spaces;
+		while (GameObject.FindGameObjectWithTag("Bat").transform.position.z > finalZ) {
+			yield return new WaitForSeconds(0F);
+			var bats = GameObject.FindGameObjectsWithTag("Bat");
+			foreach(GameObject bat in bats){
+				bat.GetComponent<Transform> ().Translate(0,0,Time.deltaTime * speed * -1);
+			}
+			/*z--;
+			if (this.transform.position.z == finalZ) {
+				this.GetComponent<Renderer> ().enabled = false;
+			}*/
+		}
+		var bats2 = GameObject.FindGameObjectsWithTag ("Bat");
+		foreach(GameObject bat in bats2){
+			bat.GetComponent<Renderer> ().enabled = false;
+		}
+	}
+
+	// other not used functions
+	// not used
+	void MoveBatLeft() {
+
+		var x = -1 * Time.deltaTime * speed;
+		//rigidbody.AddForce (x);
+		this.GetComponent<Transform> ().Translate(x,0,0);
+	}
+
+	// not used
+	void MoveBatRight() {
+
+		var x = Time.deltaTime * speed;
+		//rigidbody.AddForce (x);
+		this.GetComponent<Transform> ().Translate(x,0,0);
+	}
+}
